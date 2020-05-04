a4f009453ab21eff629ac9e4d15161439ddeed55
Victor <victorsinkunas@gmail.com>
2020-05-04 22:57:08 +0200

    Big initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <h1>Wuerk Scheduler</h1>
+    <InputHours />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import InputHours from './components/InputHours.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    InputHours,
   },
 })
+
 export default class App extends Vue {}
 </script>
 
---
+++ src/components/InputHours.vue
@@ -0,0 +1,41 @@
+<template>
+  <body>
+    <img alt="Clock" src="../assets/clock.png">
+    <p> How many hours are you working this week?</p>
+    <input v-model="message" placeholder="(Total time in hours)">
+    </body>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class InputHours extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+img {
+  width: 300px;
+  height: 300px;
+  border-radius: 50%;
+  box-shadow: 0 0 1em black;
+}
+</style>

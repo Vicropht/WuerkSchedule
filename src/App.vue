b718563dbe94499c395f6a2972bb39db4b9abc04
Victor <victorsinkunas@gmail.com>
2020-05-17 21:14:06 +0200

    add input-hours to app

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,7 @@
       <div class="d-flex align-center">
         <h2>Wuerk Scheduler</h2>
       </div>
-
       <v-spacer></v-spacer>
-
       <v-btn href="https://github.com/Vicropht/WuerkSchedule" target="_blank" text>
         <span class="mr-2">GITHUB REPO</span>
         <v-icon>mdi-open-in-new</v-icon>
@@ -15,50 +13,7 @@
 
     <v-content>
       <v-layout align-center justify-center column fill-height>
-        <v-stepper v-model="e1" dark>
-          <v-stepper-header>
-            <v-stepper-step :complete="e1 > 1" editable step="1">Input Weekly Hours</v-stepper-step>
-
-            <v-divider></v-divider>
-
-            <v-stepper-step :complete="e1 > 2" step="2">Set Hours</v-stepper-step>
-
-            <v-divider></v-divider>
-
-            <v-stepper-step step="3">Share</v-stepper-step>
-          </v-stepper-header>
-
-          <v-stepper-items>
-            <v-stepper-content step="1">
-              <v-card class="dark" max-width="400">
-                <v-card-text>
-                  <div>Hours in the week</div>
-                  <p class="display-1">How many hours will you work this week?</p>
-                  <input v-model.number="Hours" type="number">
-                </v-card-text>
-              </v-card>
-
-              <v-btn color="primary" @click="e1 = 2">Continue</v-btn>
-              <v-btn flat>Cancel</v-btn>
-            </v-stepper-content>
-
-            <v-stepper-content step="2">
-              <v-card class="mb-5" color="grey lighten-1" height="200px"></v-card>
-
-              <v-btn color="primary" @click="e1 = 3">Continue</v-btn>
-
-              <v-btn flat>Cancel</v-btn>
-            </v-stepper-content>
-
-            <v-stepper-content step="3">
-              <v-card class="mb-5" color="grey lighten-1" height="200px"></v-card>
-
-              <v-btn color="primary" @click="e1 = 1">Continue</v-btn>
-
-              <v-btn flat>Cancel</v-btn>
-            </v-stepper-content>
-          </v-stepper-items>
-        </v-stepper>
+        <input-hours />
       </v-layout>
     </v-content>
   </v-app>
@@ -66,16 +21,19 @@
 
 <script lang="ts">
 import Vue from 'vue';
+import InputHours from './components/InputHours.vue';
 
 export default Vue.extend({
   name: 'App',
 
   components: {
-    //
+    InputHours,
   },
 
   data: () => ({
-    //
+    return: {
+      e1: 0,
+    },
   }),
 });
 </script>
--- src/components/InputHours.vue
+++ src/components/InputHours.vue
@@ -1,64 +1,58 @@
 <template>
-  <v-app>
-    <v-content>
-      <v-row align="center" justify="center">
-        <v-stepper v-model="e1" dark>
-          <v-stepper-header>
-            <v-stepper-step :complete="e1 > 1" step="1">Name of step 1</v-stepper-step>
+  <v-stepper v-model="e1" dark>
+    <v-stepper-header>
+      <v-stepper-step :complete="e1 > 1" editable step="1">Input Weekly Hours</v-stepper-step>
 
-            <v-divider></v-divider>
+      <v-divider></v-divider>
 
-            <v-stepper-step :complete="e1 > 2" step="2">Name of step 2</v-stepper-step>
+      <v-stepper-step :complete="e1 > 2" step="2">Set Hours</v-stepper-step>
 
-            <v-divider></v-divider>
+      <v-divider></v-divider>
 
-            <v-stepper-step step="3">Name of step 3</v-stepper-step>
-          </v-stepper-header>
+      <v-stepper-step step="3">Share</v-stepper-step>
+    </v-stepper-header>
 
-          <v-stepper-items>
-            <v-stepper-content step="1">
-              <v-card class="mb-5" color="grey lighten-1" height="200px"></v-card>
+    <v-stepper-items>
+      <v-stepper-content step="1">
+        <v-card class="dark" max-width="400">
+          <v-card-text>
+            <div>Hours in the week</div>
+            <p class="display-1">How many hours will you work this week?</p>
+            <input v-model.number="Hours" type="number" />
+          </v-card-text>
+        </v-card>
 
-              <v-btn color="primary" @click="e1 = 2">Continue</v-btn>
+        <v-btn color="primary" @click="e1 = 2">Continue</v-btn>
+        <v-btn flat>Cancel</v-btn>
+      </v-stepper-content>
 
-              <v-btn flat>Cancel</v-btn>
-            </v-stepper-content>
+      <v-stepper-content step="2">
+        <v-card class="mb-5" color="grey lighten-1" height="200px"></v-card>
 
-            <v-stepper-content step="2">
-              <v-card class="mb-5" color="grey lighten-1" height="200px"></v-card>
+        <v-btn color="primary" @click="e1 = 3">Continue</v-btn>
 
-              <v-btn color="primary" @click="e1 = 3">Continue</v-btn>
+        <v-btn flat>Cancel</v-btn>
+      </v-stepper-content>
 
-              <v-btn flat>Cancel</v-btn>
-            </v-stepper-content>
+      <v-stepper-content step="3">
+        <v-card class="mb-5" color="grey lighten-1" height="200px"></v-card>
 
-            <v-stepper-content step="3">
-              <v-card class="mb-5" color="grey lighten-1" height="200px"></v-card>
+        <v-btn color="primary" @click="e1 = 1">Continue</v-btn>
 
-              <v-btn color="primary" @click="e1 = 1">Continue</v-btn>
-
-              <v-btn flat>Cancel</v-btn>
-            </v-stepper-content>
-          </v-stepper-items>
-        </v-stepper>
-      </v-row>
-    </v-content>
-  </v-app>
+        <v-btn flat>Cancel</v-btn>
+      </v-stepper-content>
+    </v-stepper-items>
+  </v-stepper>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Component, Vue } from 'vue-property-decorator';
 
 @Component
-export default class InputHours extends Vue {}
+export default class InputHours extends Vue {
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-img {
-  width: 300px;
-  height: 300px;
-  border-radius: 50%;
-  box-shadow: 0 0 1em black;
-}
 </style>
